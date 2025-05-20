49e26ffda038076005157a29dc0b5de04e11b978
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-05-20 13:59:27 +0200

    Third scaffold of frontend

--- frontend/webPage/src/main.tsx
+++ frontend/webPage/src/main.tsx
@@ -1,57 +1,43 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  Outlet,
-  RouterProvider,
-  createRootRoute,
-  createRoute,
-  createRouter,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
 
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
 
-import App from './App.tsx'
-
-const rootRoute = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
-})
-
-const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/',
-  component: App,
-})
-
-const routeTree = rootRoute.addChildren([indexRoute])
-
+// Create a new router instance
 const router = createRouter({
   routeTree,
-  context: {},
+  context: {
+    ...TanStackQueryProvider.getContext(),
+  },
   defaultPreload: 'intent',
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
 })
 
+// Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router
   }
 }
 
+// Render the app
 const rootElement = document.getElementById('app')
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <TanStackQueryProvider.Provider>
+        <RouterProvider router={router} />
+      </TanStackQueryProvider.Provider>
     </StrictMode>,
   )
 }
