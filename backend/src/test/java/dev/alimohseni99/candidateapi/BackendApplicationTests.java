b0ca27e5a6a4a7e45fa5ddc393f86ed71c9920d4
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-05-20 10:04:10 +0200

    Initial commit

---
+++ backend/src/test/java/dev/alimohseni99/candidateapi/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alimohseni99.candidateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
