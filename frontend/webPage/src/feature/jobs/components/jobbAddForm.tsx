a52f0e28f7af2432dec53f94bd87aac792e24649
TibbersEvolved <erik.olin.spanga@gmail.com>
2025-05-26 10:48:40 +0200

    Frontend styling, form fields use ShadCN colors

--- frontend/webPage/src/feature/jobs/components/jobbAddForm.tsx
+++ frontend/webPage/src/feature/jobs/components/jobbAddForm.tsx
@@ -25,19 +25,19 @@ export default function JobAddForm(prop: idProp) {
       <form className="flex-col flex" action={(e) => post(e)}>
         <span>Job Title</span>
         <input
-          className="bg-slate-200 border-2 border-s border-slate-300"
+          className="bg-secondary border-2 border-s border-slate-300"
           type="text"
           name="title"
         />
         <span>Link</span>
         <input
-          className="bg-slate-200 border-2 border-s border-slate-300 rounded-sm"
+          className="bg-secondary border-2 border-s border-slate-300 rounded-sm"
           type="text"
           name="link"
         />
         <button
           type="submit"
-          className="border-2 bg-emerald-500 mt-2 rounded-sm hover:bg-emerald-300"
+          className="border-2 bg-accent mt-2 rounded-sm hover:bg-emerald-300"
         >
           Create
         </button>
